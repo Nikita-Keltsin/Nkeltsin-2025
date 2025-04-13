fc74020c917c1f28a53354cc4ba96a208c2704d0
Your Name <you@example.com>
2025-04-13 12:54:09 +0300

    add file

--- LAB2-4/main.cpp
+++ LAB2-4/main.cpp
@@ -35,29 +35,61 @@ void printStack(MyStack<int>& stack) {
 }
 
 int main() {
-    int N = 0;
-    std::cout << "Введите значение N:";
-    std::cin >> N;
-    MyStack<int> stack;
+    char continueprogramm = 'Y';
+    int choice = 0;
+    while (continueprogramm == 'Y'){
+        std::cout << "1.Вывести множители для числа 3960" << std::endl;
+        std::cout << "2.Вывести множители для числа пользователя" << std::endl;
+        std::cin >> choice;
+        if (choice == 1){
+            int N = 3960;
+            MyStack<int> stack;
+            Multipliers(N, stack);
+            // Вывод по убыванию (как хранится в стеке)
+            std::cout << N << "=";
+            MyStack<int> temp = stack;
+            bool first = true;
+            while (!temp.empty()) {
+                if (!first) std::cout << " * ";
+                std::cout << temp.top_inf();
+                temp.pop();
+                first = false;
+            }
+            std::cout << std::endl;
+            // Вывод по возрастанию (перевернем стек)
+            std::cout << N << "=";
+            printStack(stack);
+            std::cout << std::endl;
+        }
+        if (choice == 2){
+            int N = 0;
+            std::cout << "Введите значение N:";
+            std::cin >> N;
+            MyStack<int> stack;
     
-    Multipliers(N, stack);
+            Multipliers(N, stack);
     
-    // Вывод по убыванию (как хранится в стеке)
-    std::cout << N << "=";
-    MyStack<int> temp = stack;
-    bool first = true;
-    while (!temp.empty()) {
-        if (!first) std::cout << " * ";
-        std::cout << temp.top_inf();
-        temp.pop();
-        first = false;
+            // Вывод по убыванию (как хранится в стеке)
+            std::cout << N << "=";
+            MyStack<int> temp = stack;
+            bool first = true;
+            while (!temp.empty()) {
+                if (!first) std::cout << " * ";
+                std::cout << temp.top_inf();
+                temp.pop();
+                first = false;
+            }
+            std::cout << std::endl;
+            // Вывод по возрастанию (перевернем стек)
+            std::cout << N << "=";
+            printStack(stack);
+            std::cout << std::endl;
+        }
+        std::cout << "Продолжить программу?(Y/N)" << std::endl;
+        std::cin >> continueprogramm;
+        if (continueprogramm != 'Y'){
+            std::cout << "До новых встреч!" << std::endl;
+        }
     }
-    std::cout << std::endl;
-    
-    // Вывод по возрастанию (перевернем стек)
-    std::cout << N << "=";
-    printStack(stack);
-    std::cout << std::endl;
-    
     return 0;
 }
